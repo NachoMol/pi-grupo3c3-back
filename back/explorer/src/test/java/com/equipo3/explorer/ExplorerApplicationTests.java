4af2c7272d41848c1ded58049177e44c0ba3b72c
Luis Muñoz <luismunozse@gmail.com>
2023-10-19 06:42:39 -0300

    inicializando proyecto

---
+++ back/explorer/src/test/java/com/equipo3/explorer/ExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipo3.explorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
