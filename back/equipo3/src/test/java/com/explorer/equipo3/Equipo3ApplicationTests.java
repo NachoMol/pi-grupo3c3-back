586f334bfb41c9dc348f2e8d1c0295eb7a7d777c
NachoMol <ignaciotmolina@gmail.com>
2023-11-23 11:53:49 -0300

    fixeando implementacion de swagger

--- back/equipo3/src/test/java/com/explorer/equipo3/Equipo3ApplicationTests.java
+++ back/equipo3/src/test/java/com/explorer/equipo3/Equipo3ApplicationTests.java
@@ -2,8 +2,10 @@ package com.explorer.equipo3;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 class Equipo3ApplicationTests {
 
 	@Test
