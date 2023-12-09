1cf996740fd04a8b0757030e1f1631d886651d3d
NachoMol <ignaciotmolina@gmail.com>
2023-12-09 20:28:47 -0300

    implementando testing con creacion update y delete. Resta eliminar categoria y city

--- back/equipo3/src/test/java/com/explorer/equipo3/Service/ProductServiceTest.java
+++ back/equipo3/src/test/java/com/explorer/equipo3/Service/ProductServiceTest.java
@@ -1,6 +1,10 @@
 package com.explorer.equipo3.Service;
 
+import com.explorer.equipo3.model.Category;
+import com.explorer.equipo3.model.City;
 import com.explorer.equipo3.model.Product;
+import com.explorer.equipo3.service.CategoryService;
+import com.explorer.equipo3.service.CityService;
 import com.explorer.equipo3.service.IProductService;
 import com.explorer.equipo3.service.ProductService;
 import org.apache.logging.log4j.LogManager;
@@ -11,6 +15,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,6 +34,17 @@ public class ProductServiceTest {
     @Autowired
     ProductService productService;
 
+    @Autowired
+    CategoryService categoryService;
+
+    @Autowired
+    CityService cityService;
+
+
+    Category categoryTest = new Category("Categoria Test","Test", "test");
+    City cityTest = new City("Test");
+    Product productTest = new Product("Test",1000.0,categoryTest,cityTest);
+
     @Test
     @Order(1)
     void getAllProducts(){
@@ -35,7 +53,58 @@ public class ProductServiceTest {
         assertTrue(getAllProductsTest.size() > 1);
     }
 
-    
+    @Test
+    @Order(2)
+    void getAllProductsPageable(){
+        logger.info("Comenzamos test de getAllProducts pageable");
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<Product> getAllProductsPageableTest = productService.getAllProductsPage(pageable);
+        assertTrue(getAllProductsPageableTest.getTotalElements() > 1);
+    }
+
+    @Test
+    @Order(3)
+    void getRandomProducts(){
+        logger.info("Comenzamos test de getRandomProducts");
+        List<Product> getRandomProductsTest = productService.getRandomProducts();
+        assertTrue(getRandomProductsTest.size() > 1);
+    }
+
+    @Test
+    @Order(4)
+    void deleteProduct(){
+
+        logger.info("Comenzamos test de delete Product");
+
+        logger.info("Primero creamos un producto");
+
+        // Guardar primero la categoría y la city
+        Category savedCategory = categoryService.saveCategory(categoryTest);
+        City savedCity = cityService.saveCity(cityTest);
+
+        // Asignar la categoría guardada al producto
+        productTest.setCategory(savedCategory);
+        productTest.setCity(savedCity);
+
+        Product saveProductTest = productService.saveProduct(productTest);
+        assertNotNull(saveProductTest);
+
+        logger.info("Luego Actualizamos el producto");
+
+        productTest.setName("Test1");
+        productTest.setPrice(2000.00);
+
+        assertEquals("Test1",productTest.getName());
+
+        logger.info("Eliminamos el producto, la categoria y la city");
+
+        logger.info("Id del product a eliminar: " + productTest.getId());
+
+        Long id = productTest.getId();
+
+        assertEquals("Producto anulado", productService.deleteProductById(id));
+
+    }
 
 
 }
